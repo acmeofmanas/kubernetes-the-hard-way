3c56fb51dc4c448aa3217179820eca2b30e67b8c
Manas Pradhan <manaspradhan@Manass-MacBook-Pro.local>
2022-02-06 17:50:09 +0800

    add update to docker install

--- vagrant/ubuntu/install-docker.sh
+++ vagrant/ubuntu/install-docker.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
-export DEBIAN_FRONTEND=noninteractive 
-apt-get update \
-    && apt-get install -y \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        software-properties-common \
-    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - \
-    && add-apt-repository \
-        "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-        $(lsb_release -cs) \
-        stable" \
-    && apt-get update \
-    && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 18.06 | head -1 | awk '{print $3}')
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" |tee /etc/apt/sources.list.d/docker.list
+
+apt update
+apt-get install docker-ce docker-ce-cli containerd.io -y 
+
+sleep 1
+cat << EOF > /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"]
+}
+EOF
+
+systemctl daemon-reload
+systemctl restart docker
